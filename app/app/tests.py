8361b60af7edfdf040bc3a6f482dcfd5ba522c3d
ugunmatchable14 <cp12umang@gmail.com>
2020-10-13 18:01:43 +0530

    Added blank lines in tests.py and calc.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
-from app.calc import Add
+
+from app.calc import add, subtract
+
 
 class CalcTests(TestCase):
 
     def test_add_numbers(self):
         """ Test that the two numbers are correctly added """
         self.assertEqual(add(3, 5), 8)
+
+    def test_subtract_numbers(self):
+        """Test that two numbers are subtracted correctly"""
+        self.assertEqual(subtract(3, 9), 6)
